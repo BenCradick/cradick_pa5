6cf2e24dfeeeedb5d116307d8fd46989e730a8b3
Ben Cradick <ben.cradick@gmail.com>
2017-11-19 14:22:29 -0600

    Completed the adding up all integer arguements part.

--- main.c
+++ main.c
@@ -1,4 +1,25 @@
-//
-// Created by Ben Cradick on 11/19/17.
-//
+/*Ben Cradick
+ * cs 2750 PA
+ * 11-19-2017
+ * */
+#include "func.h"
+#include <stdlib.h>
+int main(int argc, char *argv[]){
+    int total = 0;
+
+    if(argc > 200){
+         //capping the maximum number of command line arguments at 200
+        printf("\nToo many arguments\n");
+        return 0;
+    }
+
+    int i = 0;
+
+    for(i = 0; i < argc; i++){
+        /*strtol converts whatever it recieves as input to long int which I cast to int
+         *if an input is not an integer it returns a 0 which doesn't affect our end goal here.
+         */
+        total += (int)strtol(argv, 10);
+    }
+}
 
