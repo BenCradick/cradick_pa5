a896fc4f449f05f2f0d42ff186b5df77e9600ac8
Ben Cradick <ben.cradick@gmail.com>
2017-11-19 14:54:28 -0600

    Built the function to check future args for the current arg and print something when that happens

--- func.c
+++ func.c
@@ -3,6 +3,14 @@
 //11-19-2017
 
 #include "func.h"
+#include <cstdlib>
 void checkString(char *argv[], int argc, int current){
-
+    if(argv[current] != '0' && strtol(argv[current], 10) == 0){
+        int i;
+        for(i = current+1; i < argc; i ++){
+            if(strstr(argv[i], argv[current])){
+                printf("\n%s is a substring of %s\n", argv[current], argv[i]);
+            }
+        }
+    }
 }
