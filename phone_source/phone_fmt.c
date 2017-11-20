3054b4a22acd19879a101d76e8425b1657adf32a
Ben Cradick <ben.cradick@gmail.com>
2017-11-20 10:30:52 -0600

    added the phone_fmt.c and phone_fmt.h files

---
+++ phone_source/phone_fmt.c
@@ -0,0 +1,5 @@
+//
+// Created by Ben Cradick on 11/20/17.
+//
+
+#include "phone_fmt.h"
---
+++ phone_source/phone_fmt.h
@@ -0,0 +1,8 @@
+//
+// Created by Ben Cradick on 11/20/17.
+//
+
+#ifndef CRADICK_PA5_PHONE_FMT_H
+#define CRADICK_PA5_PHONE_FMT_H
+
+#endif //CRADICK_PA5_PHONE_FMT_H
